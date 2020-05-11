3fd1b6137df059950e2f759c1e06871f208f67b8
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2020-05-11 19:04:17 +0000

    Petites modifs

--- hark_suite.sh
+++ hark_suite.sh
@@ -3,3 +3,9 @@
 echo "Etape 6/6 : Enregistrement des resultats"
 wpa_cli -i wlan0 set update_config 1
 wpa_cli -i wlan0 save_config
+
+echo "Les resultats sont enregistree dans le fichier hark.res"
+
+macchanger -r wlan0
+
+wpa_supplicant.sh
